42cd05f1a713e6994dfceda2ddab87d6ca4ac54c
Your Name <your_email@example.com>
2026-06-07 22:33:56 +0530

    First clean upload from Android

---
+++ src/NtaMockTest.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NtaMockTest from './NtaMockTest';
+
+function App() {
+  return (
+    <div className="App">
+      <NtaMockTest />
+    </div>
+  );
+}
+
+export default App;
